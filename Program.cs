975fca88411ceb0f4ca736b113ee40f8641e9457
Chengkai Yang <yangchengkai@gmail.com>
2021-03-02 15:39:13 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using StackExchange.Redis;
+
+namespace AzureRedisSubscriber
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Redis Message subscriber.");
+
+         var redisClient = ConnectionMultiplexer.Connect("chyaredis.redis.cache.windows.net:6380,password=Q6UOzzdNSIiQa3sgw6znCeHINpfXKAStPkIEinjXths=,ssl=True,abortConnect=False");
+         var redisSubscriber = redisClient.GetSubscriber();
+
+         redisSubscriber.Subscribe("redisChannel", (x, y) =>
+                                                   {
+                                                      Console.WriteLine(y);
+                                                   });
+
+         Console.ReadKey();
+
+      }
+   }
+}
